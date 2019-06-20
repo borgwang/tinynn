43669e8d693c928102c8555ef099aaaeeb9f4e6c
borgwang <badbobobo@gmail.com>
2019-06-20 23:18:28 +0800

    test: add unit test

---
+++ test/test_utils_seeder.py
@@ -0,0 +1,23 @@
+# Author: borgwang <borgwang@126.com>
+# Date: 2019-06-20
+#
+# Filename: test_utils_seeder.py
+# Description: test unit for utils/seeder.py
+
+
+import os
+import sys
+sys.path.append(os.getcwd())
+
+import time
+
+import pytest
+
+from utils.seeder import random_seed
+
+
+def test_random_seed():
+    with pytest.raises(ValueError):
+        random_seed(2**32 + 1)
+
+
---
+++ test/test_utils_timing.py
@@ -0,0 +1,43 @@
+# Author: borgwang <borgwang@126.com>
+# Date: 2019-06-20
+#
+# Filename: test_utils_timing.py
+# Description: test unit for utils/timer.py
+
+
+import os
+import sys
+sys.path.append(os.getcwd())
+
+import time
+
+import pytest
+
+from utils.timer import Timer
+
+
+@pytest.fixture
+def test_task_timer():
+    return Timer("test_task")
+
+
+def test_timer_duration(test_task_timer):
+    test_task_timer.start()
+    time.sleep(2)
+    test_task_timer.pause()
+    time.sleep(1)
+    test_task_timer.start()
+    time.sleep(2)
+    test_task_timer.stop()
+    assert 2 * 2 <= test_task_timer.duration <= 2 * 2 + 0.1
+
+
+def test_timer_count(test_task_timer):
+    test_task_timer.start()
+    time.sleep(2)
+    test_task_timer.pause()
+    time.sleep(1)
+    test_task_timer.start()
+    time.sleep(2)
+    test_task_timer.stop()
+    assert test_task_timer.count == 2
