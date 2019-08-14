b242009b47dd0ae3aa454c9edf895e1a48a29cde
wangguibo <wangguibo@ainnovation.com>
2019-08-14 16:35:53 +0800

    update README.md

--- examples/nn_paint/README.md
+++ examples/nn_paint/README.md
@@ -4,12 +4,21 @@ Mimic a picture using a fully-connected neural. The input to the network are the
 Basically it's a regression task which neural network tries to learn the mapping between coordinate and RGB value of a specify picture. We use the [Square loss](https://en.wikipedia.org/wiki/Mean_squared_error) which leads to some kind of blurry effect.
 
 ## Example  
-Origin picture  
+Input image   
 <img src="https://github.com/borgwang/toys/raw/master/nn_paint/res/origin.jpg" width = "256" height = "160" alt="origin" align=center />  
-
-Painting  
+  
+Output  
 <img src="https://media.giphy.com/media/9xjTwYDV6zIW1aE62R/giphy.gif" width = "256" height = "160" alt="paint" align=center />   
 
 ## Architecture
 We use a 4 layers full-connected neural network and train it with Adam optimizer(learning_rate=0.0003).  
 Feel free to modify the code to build your own painter.  
+
+### How to run
+place your RGB image file in /examples/nn_paint/data
+
+```bash
+# in the root directory
+python examples/nn_paint/run.py --file_name your-img-file 
+```
+
--- examples/nn_paint/run.py
+++ examples/nn_paint/run.py
@@ -4,41 +4,42 @@
 # Filename: run.py
 # Description: Use a neural network to mimic images.
 
-import sys
-import os
-sys.path.append(os.getcwd())
+import runtime_path  # isort:skip
 
-import time
-import numpy as np
 import argparse
+import os
+import time
+
+import numpy as np
 from PIL import Image
 
-from core.nn import NeuralNet
-from core.layers import Linear, ReLU, Sigmoid
-from core.optimizer import Adam
+from core.evaluator import EVEvaluator
+from core.evaluator import MSEEvaluator
+from core.layers import Linear
+from core.layers import ReLU
+from core.layers import Sigmoid
 from core.loss import MSELoss
 from core.model import Model
-from core.evaluator import EVEvaluator, MSEEvaluator
+from core.nn import NeuralNet
+from core.optimizer import Adam
 from utils.data_iterator import BatchIterator
 
 
 def main(args):
     # data preparing
-    filename = "origin.jpg"
-    img_path = os.path.join(args.dir, filename)
+    img_path = os.path.join(args.data_dir, args.file_name)
     if not os.path.isfile(img_path):
-        raise FileExistsError("Image \"origin.jpg\" not exist in %s" % args.dir)
+        raise FileExistsError("Image %s not exist in %s" %
+                              (args.file_name, args.data_dir))
     img = np.asarray(Image.open(img_path), dtype="float32") / 255.0
 
-    train_X, train_Y = [], []
+    train_x, train_y = [], []
     h, w, _ = img.shape
     for r in range(h):
         for c in range(w):
-            train_X.append([(r-h/2.0)/h, (c-w/2.0)/w])
-            train_Y.append(img[r][c])
-
-    train_X = np.asarray(train_X)
-    train_Y = np.asarray(train_Y)
+            train_x.append([(r - h / 2.0) / h, (c - w / 2.0) / w])
+            train_y.append(img[r][c])
+    train_x, train_y = np.asarray(train_x), np.asarray(train_y)
 
     net = NeuralNet([
         Linear(2, 30),
@@ -60,33 +61,34 @@ def main(args):
     iterator = BatchIterator(batch_size=args.batch_size)
     for epoch in range(args.num_ep):
         t_start = time.time()
-        for batch in iterator(train_X, train_Y):
+        for batch in iterator(train_x, train_y):
             preds = model.forward(batch.inputs)
             loss, grads = model.backward(preds, batch.targets)
             model.apply_grad(grads)
 
         # evaluate
-        preds = net.forward(train_X)
-        ev = ev_evaluator.evaluate(preds, train_Y)
-        mse = mse_evaluator.evaluate(preds, train_Y)
+        preds = net.forward(train_x)
+        ev = ev_evaluator.evaluate(preds, train_y)
+        mse = mse_evaluator.evaluate(preds, train_y)
         print(ev, mse)
 
         if args.paint:
             # generate painting
             preds = preds.reshape(h, w, -1)
             preds = (preds * 255.0).astype("uint8")
-            output_filename = "painting.jpg"
-            Image.fromarray(preds).save(os.path.join(args.dir, output_filename))
-
+            filename, ext = os.path.splitext(args.file_name)
+            output_filename = "output" + ext
+            output_path = os.path.join(args.data_dir, output_filename)
+            Image.fromarray(preds).save(output_path)
         print("Epoch %d time cost: %.2f" % (epoch, time.time() - t_start))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dir", default="./examples/nn_paint/data", type=str)
+    parser.add_argument("--data_dir", default="./examples/nn_paint/data", type=str)
+    parser.add_argument("--file_name", default="input.jpg", type=str)
     parser.add_argument("--seed", default=0, type=int)
     parser.add_argument("--batch_size", default=32, type=int)
     parser.add_argument("--num_ep", default=100, type=int)
     parser.add_argument("--paint", default=True, type=bool)
-    args = parser.parse_args()
-    main(args)
+    main(parser.parse_args())
