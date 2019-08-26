4dd3167fcb9f0b8ab9d40b289a9f04e8016e7a32
borgwang <badbobobo@gmail.com>
2019-08-26 11:45:19 +0800

    feat: add sparse_softmax_cross_entropy_loss

--- core/losses.py
+++ core/losses.py
@@ -64,38 +64,55 @@ class HuberLoss(BaseLoss):
 
 
 class SoftmaxCrossEntropyLoss(BaseLoss):
-    """
-    L = weight[class] * (-log(exp(x[class]) / sum(exp(x))))
 
-    weight is a 1D tensor assigning weight to each of the classes.
-    """
-    def __init__(self, weight=None, sparse=True):
-        self._sparse = sparse
-        if weight is not None:
-            assert len(weight.shape) == 1
-            self._weight = np.asarray(weight)
-        else:
-            self._weight = None
+    def __init__(self, weight=None):
+        """
+        L = weight[class] * (-log(exp(x[class]) / sum(exp(x))))
+        :param weight: A 1D tensor [n_classes] assigning weight to each corresponding sample.
+        """
+        weight = np.asarray(weight) if weight is not None else weight
+        self._weight = weight
 
-    def loss(self, predicted, actual):
-        m = predicted.shape[0]
-
-        exps = np.exp(predicted - np.max(predicted))
+    def loss(self, logits, labels):
+        m = logits.shape[0]
+        exps = np.exp(logits - np.max(logits))
         p = exps / np.sum(exps)
-        if self._sparse:
-            nll = -np.log(np.sum(p * actual, axis=1))
-        else:
-            nll = -np.log(p[range(m), actual])
+        nll = -np.log(np.sum(p * labels, axis=1))
 
         if self._weight is not None:
-            nll *= self._weight[actual]
+            nll *= self._weight[labels]
         return np.sum(nll) / m
 
-    def grad(self, predicted, actual):
-        m = predicted.shape[0]
-        grad = np.copy(predicted)
-        if self._sparse:
-            grad -= actual
-        else:
-            grad[range(m), actual] -= 1.0
+    def grad(self, logits, labels):
+        m = logits.shape[0]
+        grad = np.copy(logits)
+        grad -= labels
         return grad / m
+
+
+class SparseSoftmaxCrossEntropyLoss(BaseLoss):
+
+    def __init__(self, weight=None):
+        """
+        L = weight[class] * (-log(exp(x[class]) / sum(exp(x))))
+        :param weight: A 1D tensor [n_classes] assigning weight to each corresponding sample.
+        """
+        weight = np.asarray(weight) if weight is not None else weight
+        self._weight = weight
+
+    def loss(self, logits, labels):
+        m = logits.shape[0]
+        exps = np.exp(logits - np.max(logits))
+        p = exps / np.sum(exps)
+        nll = -np.log(p[range(m), labels])
+
+        if self._weight is not None:
+            nll *= self._weight[labels]
+        return np.sum(nll) / m
+
+    def grad(self, logits, actual):
+        m = logits.shape[0]
+        grad = np.copy(logits)
+        grad[range(m), actual] -= 1.0
+        return grad / m
+
