2d793f2dba5da2abdf0e7972cb20557e7249a926
borgwang <badbobobo@gmail.com>
2019-10-15 12:48:53 +0800

    add md5 checksum for dataset file

--- utils/dataset.py
+++ utils/dataset.py
@@ -12,10 +12,12 @@ from utils.downloader import download_url
 
 def mnist(data_dir):
     url = "http://deeplearning.net/data/mnist/mnist.pkl.gz"
+    checksum = "a02cd19f81d51c426d7ca14024243ce9"
+
     save_path = os.path.join(data_dir, url.split("/")[-1])
     print("Preparing MNIST dataset ...")
     try:
-        download_url(url, save_path)
+        download_url(url, save_path, checksum)
     except Exception as e:
         print("Error downloading dataset: %s" % str(e))
         sys.exit(1)
--- utils/downloader.py
+++ utils/downloader.py
@@ -1,5 +1,6 @@
 """Simple utilities to download and save a file with progress bar."""
 
+import hashlib
 import os
 from urllib.error import URLError
 from urllib.request import urlretrieve
@@ -10,22 +11,30 @@ def show_progress(blk_num, blk_sz, tot_sz):
     print("Progress: %.1f %%" % percentage, end="\r", flush=True)
 
 
-def download_url(url, file_path):
+def md5_checksum(file_path):
+    with open(file_path, "rb") as f:
+        checksum = hashlib.md5(f.read()).hexdigest()
+    return checksum
+
+
+def download_url(url, file_path, checksum):
     # create directory if needed
-    Dir = os.path.dirname(file_path)
-    if not os.path.exists(Dir):
-        os.makedirs(Dir)
-    # download
-    try:
-        if os.path.exists(file_path):
+    file_dir = os.path.dirname(file_path)
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+
+    if os.path.exists(file_path):
+        # check md5
+        if md5_checksum(file_path) == checksum:
             print("{} already exists.".format(file_path))
+            return 
         else:
-            print("Downloading {} to {}".format(url, file_path))
-            try:
-                urlretrieve(url, file_path, show_progress)
-            except URLError:
-                raise RuntimeError("Error downloading resource!")
-            finally:
-                print()
+            print("Wrong checksum!")
+
+    try:
+        print("Downloading {} to {}".format(url, file_path))
+        urlretrieve(url, file_path, show_progress)
+    except URLError:
+        raise RuntimeError("Error downloading resource!")
     except KeyboardInterrupt:
         print("Interrupted")
