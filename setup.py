08d8692817b43542fe5d283279bee8468ea5d7da
borgwang <badbobobo@gmail.com>
2021-10-29 15:28:43 +0800

    v0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tinynn",
-    version="0.1.0",
+    version="0.1.1",
     author="borgwang",
     author_email="borgwang@126.com",
     description="A lightweight deep learning library",
